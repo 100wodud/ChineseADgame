987ba4733dd11942760a03e0bcce34fa2cc4d23f
shw0514 <gusdnddl50@gmail.com>
2024-02-26 15:00:29 +0900

    ADD - Player 이동 구현
    PlayerController.cs 생성
    PlayerInputController.cs 생성
    PlayerMovement.cs 생성
    
    작동 확인 완료
    (좌우 움직임만 가능하게끔 위아래키는 ws가 아닌 별도의 키 지정)

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _controller = GetComponent<PlayerController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(_movementDirection);
+    }
+    private void Move(Vector2 direciton)
+    {
+        _movementDirection = direciton;
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 1.5f;
+
+        _rigidbody.velocity = direction;
+    }
+
+
+}
