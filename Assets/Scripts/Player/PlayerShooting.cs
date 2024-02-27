d8ebabbdff27308fbc6c03ae2621411713bbe535
shw0514 <gusdnddl50@gmail.com>
2024-02-27 19:48:50 +0900

    Fixed - 투사체 구현

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -2,34 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour
+public class TopDownShooting : MonoBehaviour
 {
+    private PlayerController _contoller;
+    private ProjectileManager _projectileManager;
+    private Vector2 _aimDirection = Vector2.up;
 
     [SerializeField] private Transform projectileSpawnPosition;
+    private Transform aimPoint;
 
-    public GameObject testPrefab;
-
-    private void Start()
+    private void Awake()
     {
-        Attack();
+        _contoller = GetComponent<PlayerController>();
     }
 
-    void Attack()
+    // Start is called before the first frame update
+    void Start()
     {
-        StartCoroutine("OnShoot");
+        _projectileManager = ProjectileManager.instance;
+        _contoller.OnAttackEvent += OnShoot;
     }
 
-    IEnumerator OnShoot()
+    private void OnAim(Vector2 newAimDirection)
     {
-        while (true)
+        aimPoint.position = newAimDirection;
+    }
+
+    private void OnShoot(AttackSO attackSO)
+    {
+        RangedAttackData rangedAttackData = attackSO as RangedAttackData;
+        float projectilesAngleSpace = rangedAttackData.multipleProjectilesAngel;
+        int numberOfProjectilesPerShot = rangedAttackData.numberofProjectilesPerShot;
+
+        float minAngle = -(numberOfProjectilesPerShot / 2f) * projectilesAngleSpace + 0.5f * rangedAttackData.multipleProjectilesAngel;
+
+
+        for (int i = 0; i < numberOfProjectilesPerShot; i++)
         {
-            CreateProjectile();
-            yield return new WaitForSeconds(0.5f);
+            float angle = minAngle + projectilesAngleSpace * i;
+            float randomSpread = Random.Range(-rangedAttackData.spread, rangedAttackData.spread);
+            angle += randomSpread;
+            CreateProjectile(rangedAttackData, angle);
         }
     }
 
-    private void CreateProjectile()
+    private void CreateProjectile(RangedAttackData rangedAttackData, float angle)
     {
-        Instantiate(testPrefab, projectileSpawnPosition.position, Quaternion.identity);
+        _projectileManager.ShootBullet(
+            projectileSpawnPosition.position,
+            RotateVector2(_aimDirection, angle),
+            rangedAttackData
+            );
+    }
+
+    private static Vector2 RotateVector2(Vector2 v, float degree)
+    {
+        return Quaternion.Euler(0, 0, degree) * v;
     }
 }
