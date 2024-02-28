9791faa3dc3c2d489c51993cd120bf14c35f605d
LeeGunHyung <154494501+Woodong96@users.noreply.github.com>
2024-02-28 20:11:12 +0900

    Feat 아이템 태그 > 레이어마스크로 수정

--- Assets/Scripts/Items/PickupItem.cs
+++ Assets/Scripts/Items/PickupItem.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public abstract class PickupItem: MonoBehaviour
 {
+
+    [SerializeField] private bool destroyOnPickup = true;
+    [SerializeField] private LayerMask canBePickupBy;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (canBePickupBy.value == (canBePickupBy.value | (1 << other.gameObject.layer)))
         {
 
             Debug.Log("충돌했다!");
             OnPickedUp(other.gameObject);
-            DestroyAllItemsWithTag("Item");
+            if (destroyOnPickup)
+            {
+                DestroyAllItemsWithTag("Item");
+            }
+
         }
     }
 
