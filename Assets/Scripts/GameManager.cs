a5c2d0d14e7a48c791a1691ab0e4b91a7708f3a6
Jaeyoung Paik <100wodud@gmail.com>
2024-02-28 20:04:37 +0900

    [fix] SpawnPoint 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -175,9 +175,9 @@ public class GameManager : MonoBehaviour
             int ranItem3 = Random.Range(0, 3);
 
 
-            Vector3 secondItemPosition = spawnPoints[0].position + new Vector3(2f, 0f, 0f);
+            Vector3 secondItemPosition = spawnPoints[2].position + new Vector3(2f, 0f, 0f);
             Instantiate(itemObjs[ranItem2], secondItemPosition, spawnPoints[3].rotation);
-            Vector3 thirdItemPosition = spawnPoints[0].position + new Vector3(-2f, 0f, 0f);
+            Vector3 thirdItemPosition = spawnPoints[2].position + new Vector3(-2f, 0f, 0f);
             Instantiate(itemObjs[ranItem3], thirdItemPosition, spawnPoints[3].rotation);
         }
 
