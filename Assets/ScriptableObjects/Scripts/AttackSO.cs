df259d1076c32f0a302827a68b8d292a75a5b6ed
shw0514 <gusdnddl50@gmail.com>
2024-02-29 20:03:13 +0900

    Fixed - 코드정리
    Player의 공격 관련 불필요한 코드 제거

--- Assets/ScriptableObjects/Scripts/AttackSO.cs
+++ Assets/ScriptableObjects/Scripts/AttackSO.cs
@@ -9,7 +9,6 @@ public class AttackSO : ScriptableObject
     [Header("Attack Info")]
     public float size;
     public float delay;
-    public float power;
     public float speed;
     public LayerMask target;
 }
